0dfd8135a5f0034a76686aab604f191f56f52212
Joshua Demirjian <demirjian101@outlook.com>
2022-04-29 18:07:46 -0400

    wrote Js check functions based upon video tutorial

--- tm_proj/P1_FV/script2.js
+++ tm_proj/P1_FV/script2.js
@@ -19,11 +19,14 @@ function showSuccess(input) {
 }
 
 // Check Email is Valid
-function isValidEmail(email) {
+function checkEmail(input) {
     const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    return emailPattern.test(email);
+    if(emailPattern.test(input.value.trim())) {
+        showSuccess(input);
+    } else {
+        showError(input, 'Email is not valid');
+    }
 }
-
 
 //Check required fields
 function checkRequired(inputArr){
@@ -35,7 +38,24 @@ function checkRequired(inputArr){
             showSuccess(input);
         }
     }); //loops through each item in the array
+}
 
+//Check input length
+function checkLength(input, min, max) {
+    if(input.value.length < min) {
+        showError(input, `${getFieldName(input)} must be at least ${min} characters.`);
+    } else if(input.value.length > max) {
+        showError(input, `${getFieldName(input)} must be less than ${max} characters.`);
+    } else {
+        showSuccess(input);
+    }
+}
+
+//check passwords match
+function checkPasswordsMatch(input1, input2) {
+    if(input1.value !== input2.value) {
+        showError(input2, 'Passwords do not match');
+    }
 }
 
 //Get FieldName
@@ -48,4 +68,8 @@ form.addEventListener('submit', function(e) {
     e.preventDefault();
 
     checkRequired([username, email, password, password2]);
+    checkLength(username, 3, 15);
+    checkLength(password, 6, 25);
+    checkEmail(email);
+    checkPasswordsMatch(password, password2);
 })
